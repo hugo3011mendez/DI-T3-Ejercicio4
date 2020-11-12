a0dccd7bb33ceb5dee822700174fe6423e304941
Hugo Mendez <hugo3011mendez@gmail.com>
2020-11-12 20:16:36 +0100

    Agregar archivos de proyecto.

---
+++ Ejercicio4/Form1.Designer.cs
@@ -0,0 +1,99 @@
+﻿namespace Ejercicio4
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtSeleccionarDir = new System.Windows.Forms.TextBox();
+            this.txtInfo = new System.Windows.Forms.TextBox();
+            this.btnSeleccionarDir = new System.Windows.Forms.Button();
+            this.lblSeleccionarDir = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // txtSeleccionarDir
+            // 
+            this.txtSeleccionarDir.Location = new System.Drawing.Point(193, 12);
+            this.txtSeleccionarDir.Name = "txtSeleccionarDir";
+            this.txtSeleccionarDir.Size = new System.Drawing.Size(568, 20);
+            this.txtSeleccionarDir.TabIndex = 0;
+            this.txtSeleccionarDir.KeyUp += new System.Windows.Forms.KeyEventHandler(this.txtSeleccionarDir_KeyUp);
+            // 
+            // txtInfo
+            // 
+            this.txtInfo.Font = new System.Drawing.Font("Courier New", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtInfo.Location = new System.Drawing.Point(193, 81);
+            this.txtInfo.Multiline = true;
+            this.txtInfo.Name = "txtInfo";
+            this.txtInfo.Size = new System.Drawing.Size(568, 345);
+            this.txtInfo.TabIndex = 1;
+            // 
+            // btnSeleccionarDir
+            // 
+            this.btnSeleccionarDir.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F);
+            this.btnSeleccionarDir.Location = new System.Drawing.Point(12, 48);
+            this.btnSeleccionarDir.Name = "btnSeleccionarDir";
+            this.btnSeleccionarDir.Size = new System.Drawing.Size(142, 80);
+            this.btnSeleccionarDir.TabIndex = 2;
+            this.btnSeleccionarDir.Text = "Seleccionar Directorio";
+            this.btnSeleccionarDir.UseVisualStyleBackColor = true;
+            this.btnSeleccionarDir.Click += new System.EventHandler(this.btnSeleccionarDir_Click);
+            // 
+            // lblSeleccionarDir
+            // 
+            this.lblSeleccionarDir.AutoSize = true;
+            this.lblSeleccionarDir.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.lblSeleccionarDir.Location = new System.Drawing.Point(83, 12);
+            this.lblSeleccionarDir.Name = "lblSeleccionarDir";
+            this.lblSeleccionarDir.Size = new System.Drawing.Size(104, 20);
+            this.lblSeleccionarDir.TabIndex = 3;
+            this.lblSeleccionarDir.Text = "Escribe aquí :";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lblSeleccionarDir);
+            this.Controls.Add(this.btnSeleccionarDir);
+            this.Controls.Add(this.txtInfo);
+            this.Controls.Add(this.txtSeleccionarDir);
+            this.Name = "Form1";
+            this.Text = "Ejercicio 4";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtSeleccionarDir;
+        private System.Windows.Forms.TextBox txtInfo;
+        private System.Windows.Forms.Button btnSeleccionarDir;
+        private System.Windows.Forms.Label lblSeleccionarDir;
+    }
+}
+
---
+++ Ejercicio4/Form1.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace Ejercicio4
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        // Voy a gestionar el evento KeyUp del TextBox para cambiar el texto del botón
+        private void txtSeleccionarDir_KeyUp(object sender, KeyEventArgs e)
+        {
+            if (txtSeleccionarDir.Text.StartsWith("%") && txtSeleccionarDir.Text.EndsWith("%"))
+            {
+                btnSeleccionarDir.Text = "Escribir Variable de Entorno";
+            }
+            else
+            {
+                btnSeleccionarDir.Text = "Seleccionar Directorio";
+            }
+        }
+
+        private void btnSeleccionarDir_Click(object sender, EventArgs e)
+        {
+            if (txtSeleccionarDir.Text.StartsWith("%") && txtSeleccionarDir.Text.EndsWith("%"))
+            {
+
+            }
+            else
+            {
+                // Guardo la ruta en una variable y establezco el directorio actual de trabajo
+                String rutaDirectorio = txtSeleccionarDir.Text;
+                Directory.SetCurrentDirectory(rutaDirectorio);
+
+                DirectoryInfo raiz = new DirectoryInfo(Directory.GetCurrentDirectory());
+
+                // Reinicio el TextBox
+                txtInfo.Text = "Subdirectorios y archivos que contiene el directorio " + raiz.Name + ":" + Environment.NewLine;
+
+                // Escribo los subdirectorios
+                foreach (DirectoryInfo dir in raiz.GetDirectories())
+                {
+                    txtInfo.AppendText(dir.Name + " \\" + Environment.NewLine);
+                }
+
+                foreach (FileInfo archivo in raiz.GetFiles())
+                {
+                    txtInfo.AppendText(archivo.Name + Environment.NewLine);
+                }
+            }
+        }
+    }
+}
